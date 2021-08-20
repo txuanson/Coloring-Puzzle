3003fa451e570eb40d4f155128a492496eb36d4c
HMy2912 <danghoanmy.2912@gmail.com>
2021-08-20 14:07:02 +0700

    update first

---
+++ main_GUI.py
@@ -0,0 +1,136 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import numpy as np
+import time
+from test import pySAT_coloring
+from test import BF_coloring
+from test import Backtracking_coloring
+
+
+class GUI(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        master.title("Coloring Puzzle")
+        master.minsize(640, 400)
+        self.buttons(master)
+        self.grid(row=0, column=0)
+
+    def buttons(self, master):
+        self.labelFrame = Label(
+            master, text='Choose file', font=('Pursia', 11))
+        self.labelFrame.place(x=40, y=20)
+        self.path_file = StringVar(value='')
+        self.label = Entry(master, textvariable=self.path_file,
+                           width=45, font=('Pursia', 11))
+        self.label.place(x=150, y=20)
+        self.button = Button(
+            master, text="Browse", command=self.fileDialog, font=('Pursia', 11))
+        self.button.place(x=550, y=15)
+
+        self.value_options = {'PySAT': '1', 'A Star': '2',
+                              'Brute Force': '3', 'Backtracking': '4'}
+        self.getAlgo = StringVar(master, '1')
+
+        i = 1
+        for (text, value) in self.value_options.items():
+            Radiobutton(master, text=text, variable=self.getAlgo,
+                        value=value).place(x=(100 * i) + 40, y=50)
+            i += 1
+
+        self.start_button = Button(
+            master, text="Start", width=60, command=self.checkType, font=('Pursia', 11))
+        self.start_button.place(x=40, y=110)
+
+        self.running_time = StringVar(master, 0)
+        self.timeFrame = Label(
+            master, text='Time', font=('Pursia', 11))
+        self.timeFrame.place(x=40, y=80)
+
+        self.time = Entry(master, textvariable=self.running_time,
+                          width=45, font=('Pursia', 11))
+        self.time.place(x=150, y=80)
+
+    def fileDialog(self):
+        self.time.configure(textvariable=0)
+        self.filename = filedialog.askopenfilename(
+            initialdir="/", title="Select A File", filetype=(('text files', 'txt'), ("all files", "*.*")))
+        self.path_file.set(self.filename)
+        self.label.configure(textvariable=self.path_file)
+        openFile = open(self.filename, 'r')
+        lines = openFile.readline().strip()
+        lines = openFile.readlines()
+        lines_int = [list(map(int, line.strip().split(' '))) for line in lines]
+        self.boardInput = np.array(lines_int)
+
+    def checkType(self):
+        print(self.getAlgo.get())
+        if self.getAlgo.get() == '1':
+            t0 = time.time()
+            self.time.configure(textvariable='Running')
+            self.result = pySAT_coloring(self.boardInput)
+            t1 = time.time()
+            self.running_time.set(t1 - t0)
+            self.time.configure(textvariable=self.running_time)
+            self.initialDraw()
+        elif self.getAlgo.get() == '3':
+            t0 = time.time()
+            self.time.configure(textvariable='Running')
+            self.result = BF_coloring(self.boardInput)
+            t1 = time.time()
+            self.running_time.set(t1 - t0)
+            self.time.configure(textvariable=self.running_time)
+            if self.result is not None:
+                self.initialDraw()
+        elif self.getAlgo.get() == '4':
+            t0 = time.time()
+            self.time.configure(textvariable='Running')
+            self.result = Backtracking_coloring(self.boardInput)
+            t1 = time.time()
+            self.running_time.set(t1 - t0)
+            self.time.configure(textvariable=self.running_time)
+            if self.result is not None:
+                self.initialDraw()
+
+    def initialDraw(self):
+        self.boardSize = 20 * 2 + 30 * len(self.boardInput)
+        self.sqSize = self.boardSize / len(self.boardInput)
+
+        mainframe = ttk.Frame(self, padding=(20, 150, 20, 20))
+        mainframe.grid(column=0, row=0, sticky=(N, S, E, W))
+
+        self.mainboard = Canvas(
+            mainframe, width=self.boardSize - 1, height=self.boardSize - 1, bg='white')
+
+        self.mainboard.grid(row=1, column=0)
+
+        for row in range(len(self.boardInput)):
+            for col in range(len(self.boardInput)):
+                top = row * self.sqSize
+                left = col * self.sqSize
+                bottom = row * self.sqSize + self.sqSize
+                right = col * self.sqSize + self.sqSize
+
+                color = 'green'
+                if self.result[row][col] == 0:
+                    color = 'red'
+
+                valueOnBoard = ''
+                if self.boardInput[col][row] != -1:
+                    valueOnBoard = self.boardInput[col][row]
+
+                self.mainboard.create_text(
+                    top + self.sqSize / 2, left + self.sqSize / 2, text=valueOnBoard, font=('Pursia', 11))
+
+                rect = self.mainboard.create_rectangle(
+                    left, top, right, bottom, outline='gray', fill=color)
+                # print(self.boardInput[col][row], end=' ')
+                self.mainboard.lower(rect)
+            # print()
+
+        self.mainboard.focus_set()
+
+
+tk = Tk()
+root = GUI(tk)
+root.mainloop()
---
+++ main_algo.py
@@ -0,0 +1,195 @@
+import numpy as np
+from pysat.solvers import Glucose3
+import itertools
+from read_input import *
+import time
+
+
+SWAP_SIGN = -1
+board = readFile("input_10x10.txt")
+print(board)
+
+# n là len(board) số dòng
+# m là len(board[0]) số cột
+
+
+def gen_moves(i, j, n, size):
+    if i == 0 and j == 0:                                                         # top-left corner
+        return [0, 1, size, size + 1]
+    elif i == 0 and j == size - 1:                                                # top-right corner
+        return [-1, 0, size - 1, size]
+    elif i == n - 1 and j == 0:                                                # bot-left corner
+        return [-size, -size + 1, 0, 1]
+    elif i == n - 1 and j == size - 1:                                         # bot-right corner
+        return [-size - 1, -size, -1, 0]
+    elif i == 0:                                                                  # top edge
+        return [-1, 0, +1, size - 1, size, size + 1]
+    elif i == n - 1:                                                           # bot edge
+        return [-size - 1, -size, -size + 1, -1, 0, 1]
+    elif j == 0:                                                                  # left edge
+        return [-size, -size + 1, 0, 1, size, size + 1]
+    elif j == size - 1:                                                           # right edge
+        return [-size - 1, -size, - 1, 0, size - 1, size]
+    else:                                                                         # center
+        return [-size - 1, -size, -size + 1, -1, 0, 1, size - 1, size, size + 1]
+
+
+def get_clause(i, j, value, size, n):
+    position = i * size + j + 1
+    moves = gen_moves(i, j, n, size)
+    clause = []
+    if value > len(moves):
+        for move in moves:
+            clause.append([position + move])
+    else:
+        t = (position + np.array(moves))
+        r = list(itertools.combinations(t, value + 1))
+        cl = np.asarray(r)
+        clause.extend(cl * -1)
+        temp = position + np.array(moves)
+        r = list(itertools.combinations(temp, len(moves) - value + 1))
+        cl = np.asarray(r)
+        clause.extend(cl)
+    return clause
+
+
+def init_clause(board):
+    clauses = []
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] >= 0:
+                clauses += get_clause(i, j,
+                                      board[i][j], len(board[0]), len(board))
+    # print('#clauses:', clauses)
+    return clauses
+
+
+def pySAT_coloring(board):
+    clauses = init_clause(board)
+    g = Glucose3()
+    for it in clauses:
+        g.add_clause([int(k) for k in it])
+    # print(g.solve())
+    g.solve()
+    model = g.get_model()
+    # print(model)
+    # print(board, end='\n')
+
+    pySAT_result = np.ones((len(board), len(board[0])))
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i * len(board[0]) + j + 1 not in model:
+                pySAT_result[i][j] = 0
+    return pySAT_result
+
+
+# result = pySAT_coloring(readFile("input_10x10.txt"))
+
+
+def Brute_force(CNF):
+    literals = set()
+    for conj in CNF:
+        for disj in conj:
+            literals.add(abs(disj))
+    literals = list(literals)
+    n = len(literals)
+    for seq in itertools.product([1, -1], repeat=n):
+        a = np.asarray(seq) * np.asarray(literals)
+        if all([bool(set(disj).intersection(set(a))) for disj in CNF]):
+            return True, a
+    return False, None
+
+
+# a = Brute_force(init_clause(board))
+# print(a)
+
+
+def BF_coloring(board):
+    TF, model = Brute_force(init_clause(board))
+
+    BF_result = np.ones((len(board), len(board[0])))
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i * len(board[0]) + j + 1 not in model:
+                BF_result[i][j] = 0
+    return BF_result
+
+
+# print(BF_coloring(board))
+
+def select_literal(cnf):
+    for c in cnf:
+        for literal in c:
+            return literal
+
+
+def dpll(cnf, assignments=[]):
+    if len(cnf) == 0:
+        return True, assignments
+
+    if any([len(c) == 0 for c in cnf]):
+        return False, None
+
+    l = select_literal(cnf)
+    new_cnf = [c for c in cnf if l not in c]
+    temp = []
+    for c in new_cnf:
+        if -l in c:
+            c = c[c != -l]
+        temp.append(c)
+    t = assignments.copy()
+    t.append(l)
+    sat, vals = dpll(temp, t)
+    if sat:
+        return sat, vals
+    else:
+        t.remove(l)
+
+    new_cnf = [c for c in cnf if -l not in c]
+    temp = []
+    for c in new_cnf:
+        if l in c:
+            c = c[c != l]
+        temp.append(c)
+    t = assignments.copy()
+    t.append(-l)
+    sat, vals = dpll(temp, t)
+    if sat:
+        return sat, vals
+    else:
+        t.remove(-l)
+    return False, None
+
+
+def Backtracking_coloring(board):
+    sol, model = dpll(init_clause(board))
+
+    Backtracking_result = np.ones((len(board), len(board[0])))
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i * len(board[0]) + j + 1 not in model:
+                Backtracking_result[i][j] = 0
+    return Backtracking_result
+
+
+# Brute Force
+# print('Brute Force')
+# t0 = time.time()
+# print(BF_coloring(board))
+# t1 = time.time()
+# print('Time:', t1 - t0)
+
+# pySAT
+print('pySAT')
+t2 = time.time()
+print(pySAT_coloring(board))
+t3 = time.time()
+print('Time:', t3 - t2)
+
+# Backtracking
+# print('Backtracking')
+# t4 = time.time()
+# print(Backtracking_coloring(board))
+# t5 = time.time()
+# print('Time:', t5 - t4)
